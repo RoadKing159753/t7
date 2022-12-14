ef265a70ad8112cbac19d8f14ea2e7000c518dd6
RoadKing159753 <111482881+RoadKing159753@users.noreply.github.com>
2022-12-14 09:15:26 +0530

    Create test.tf

---
+++ test.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+}
+ # required_version = ">= 0.14.9"
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+
+
+
+resource "aws_s3_bucket" "unrestricted" {
+  #zpc-skip-policy: ZS-AWS-00034:testing
+  bucket = "examplebuckettftest"
+  acl    = "private"
+  versioning {
+    enabled = true
+  }
+  logging {
+    target_bucket = "pavantestbucket"
+    target_prefix = "log/"
+    }
+}
+}
